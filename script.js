274cd9dda705242d001653faf78dbe031a7d1013
edwardfclark <edwardfclark@gmail.com>
2018-09-21 12:58:49 +0100

    Fixed some bugs, added some functionality
    - Added tooltips.
    - Changed some styles.
    - Added a text label to the righthand side.

--- script.js
+++ script.js
@@ -14,8 +14,6 @@ document.addEventListener("DOMContentLoaded", function() {
         const dataset = json.data;
 
         //These are the constants used for the graph's dimensions and scale.
-        
-
         const w = 1000;
         const h = 500;
         const padding = 80;
@@ -24,17 +22,18 @@ document.addEventListener("DOMContentLoaded", function() {
         const minGDP = d3.min(dataset, (d) => d[1]);
         const maxGDP = d3.max(dataset, (d) => d[1]);
         const barWidth = 4;
-
         const yScale = d3.scaleLinear().domain([minGDP, maxGDP]).range([padding, h - padding]);
         const xScale = d3.scaleLinear().domain([0, dataset.length]).range([padding, w - padding]);
-
         const dateScale = d3.scaleLinear().domain([minDate, maxDate]).range([padding, w - padding]);
         const xAxis = d3.axisBottom(dateScale).tickFormat(d3.format("d"));
         const yAxis = d3.axisLeft(yScale);
 
         //The svg element, to which bars will be appended.
         const svg = d3.select("body").append("svg").attr("width", w).attr("height", h);
+        //The div element used to show the #tooltip element.
+        const title = svg.append("title").attr("id", "tooltip").style("opacity", 0).attr("title", "regular tooltip");
 
+        //The individual bar elements.
         d3.select("svg").selectAll("rect")
         .data(dataset)
         .enter()
@@ -43,25 +42,46 @@ document.addEventListener("DOMContentLoaded", function() {
         .attr("y", (d) => h - yScale(d[1]))
         .attr("width", barWidth)
         .attr("height", (d) => yScale(d[1]) - padding)
-        .attr("class", "bar");
+        .attr("class", "bar")
+            .on("mouseover", (d, i) => {
+                title.transition()
+                    .duration(200)
+                    .text(
+                        `${d[0]}, $${d[1]} Billion`
+                    );
+            })
+            .on("mouseout", (d) => {
+                title.transition()
+                    .style("opacity", 0);
+            });
 
+        //The X Axis
         svg.append("g")
         .attr("transform", "translate(0," + (h - padding) + ")")
+        .attr("id", "x-axis")
         .call(xAxis);
 
+        //The Y Axis
         svg.append("g")
         .attr("transform", "translate("+(padding)+", 0)")
+        .attr("id", "y-axis")
         .call(yAxis);
 
+        //The Title
         svg.append("text")
         .attr("x", w/2)
         .attr("y", 35)
         .attr("text-anchor", "middle")
-        .attr("class", "title")
+        .attr("id", "title")
         .text("United States GDP");
 
-        //This is just for testing purposes at the minute.
-        //document.getElementById("content").innerHTML = minDate;
+        //The Left-Hand Label
+        svg.append("text")
+        .attr("x", -padding-80)
+        .attr("y", padding+30)
+        .attr("text-anchor", "middle")
+        .attr("class", "left-label")
+        .text("Gross Domestic Product");
 
         
     }
